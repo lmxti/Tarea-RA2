e8acb76bd6c2163a7aabbf884d793593f533646d
NikoGB <114112855+NikoGB@users.noreply.github.com>
2023-05-28 22:57:41 -0400

    Update Client.cpp

--- Client.cpp
+++ Client.cpp
@@ -4,41 +4,66 @@
 
 #include <cstring>
 #include <iostream>
+#include <regex>
+
 using namespace std;
 
-int main() {
-    
-    int sock = 0, valread;
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        cout << "Argumentos insuficientes. Para usar: ./Client IP Puerto" << endl;
+        return 1; 
+    } else {
+        string ipPattern = R"(\b(?:\d{1,3}\.){3}\d{1,3}\b)";  
+        string portPattern = R"(\b\d{1,5}\b)";              
+
+        regex ipRegex(ipPattern);
+        regex portRegex(portPattern);
+
+        if (!regex_match(string(argv[1]), ipRegex)) {
+            cout << "formato IP invalido" << endl;
+            return 1;
+        }
+
+        if (!regex_match(string(argv[2]), portRegex)) {
+            cout << "formato Puerto invalido" << endl;
+            return 1;
+        }
+    }
+
+    int sock = 0;
     struct sockaddr_in serv_addr;
-    char buffer[1024] = {0};
+    char buffer[10048] = {0};
 
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        cout << "\n Socket creation error \n";
+        cout << "\n Error en la creacion del socket \n";
         return -1;
     }
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(8080);
+    serv_addr.sin_port = htons(stoi(argv[2]));
 
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        cout << "\nInvalid address/ Address not supported \n";
+    if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0) {
+        cout << "\nIp no valida\n";
         return -1;
     }
     if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        cout << "\nConnection Failed \n";
+        cout << "\nFAllo la conexion \n";
         return -1;
     }
 
     while (true) {
         memset(buffer, 0, sizeof(buffer));
-        valread = read(sock, buffer, sizeof(buffer));
-        cout << "Server : " << buffer << endl;
+        if (read(sock, buffer, sizeof(buffer)) == 0) {
+            cout << "Servidor desconectado" << endl;
+            break;
+        }
 
+        cout << buffer;
         memset(buffer, 0, sizeof(buffer));
-        cout << "Client : ";
-        cin.getline(buffer, 1024);
+
+        cin.getline(buffer, 10048);
         send(sock, buffer, strlen(buffer), 0);
-        
+
+        system("clear");
     }
     return 0;
 }
